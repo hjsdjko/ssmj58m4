577640b6b643b073d0e6cf3c0eda228e3378e372
jsxs <2261203961@qq.com>
2024-05-12 08:22:23 +0800

    first commit

---
+++ ssmj58m4/src/main/java/com/entity/ZixishiEntity.java
@@ -0,0 +1,218 @@
+package com.entity;
+
+import com.baomidou.mybatisplus.annotations.TableId;
+import com.baomidou.mybatisplus.annotations.TableName;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.lang.reflect.InvocationTargetException;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.apache.commons.beanutils.BeanUtils;
+import com.baomidou.mybatisplus.annotations.TableField;
+import com.baomidou.mybatisplus.enums.FieldFill;
+import com.baomidou.mybatisplus.enums.IdType;
+
+
+/**
+ * 自习室
+ * 数据库通用操作实体类（普通增删改查）
+ * @author 
+ * @email 
+ * @date 2022-02-24 12:10:04
+ */
+@TableName("zixishi")
+public class ZixishiEntity<T> implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+
+	public ZixishiEntity() {
+		
+	}
+	
+	public ZixishiEntity(T t) {
+		try {
+			BeanUtils.copyProperties(this, t);
+		} catch (IllegalAccessException | InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * 主键id
+	 */
+	@TableId
+	private Long id;
+	/**
+	 * 名称
+	 */
+					
+	private String mingcheng;
+	
+	/**
+	 * 图片
+	 */
+					
+	private String tupian;
+	
+	/**
+	 * 规模
+	 */
+					
+	private String guimo;
+	
+	/**
+	 * 位置
+	 */
+					
+	private String weizhi;
+	
+	/**
+	 * 详情
+	 */
+					
+	private String xiangqing;
+	
+	/**
+	 * 价格
+	 */
+					
+	private Float price;
+	
+	/**
+	 * 座位总数
+	 */
+					
+	private Integer number;
+	
+	/**
+	 * 已选座位[用,号隔开]
+	 */
+					
+	private String selected;
+	
+	
+	@JsonFormat(locale="zh", timezone="GMT+8", pattern="yyyy-MM-dd HH:mm:ss")
+	@DateTimeFormat
+	private Date addtime;
+
+	public Date getAddtime() {
+		return addtime;
+	}
+	public void setAddtime(Date addtime) {
+		this.addtime = addtime;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	/**
+	 * 设置：名称
+	 */
+	public void setMingcheng(String mingcheng) {
+		this.mingcheng = mingcheng;
+	}
+	/**
+	 * 获取：名称
+	 */
+	public String getMingcheng() {
+		return mingcheng;
+	}
+	/**
+	 * 设置：图片
+	 */
+	public void setTupian(String tupian) {
+		this.tupian = tupian;
+	}
+	/**
+	 * 获取：图片
+	 */
+	public String getTupian() {
+		return tupian;
+	}
+	/**
+	 * 设置：规模
+	 */
+	public void setGuimo(String guimo) {
+		this.guimo = guimo;
+	}
+	/**
+	 * 获取：规模
+	 */
+	public String getGuimo() {
+		return guimo;
+	}
+	/**
+	 * 设置：位置
+	 */
+	public void setWeizhi(String weizhi) {
+		this.weizhi = weizhi;
+	}
+	/**
+	 * 获取：位置
+	 */
+	public String getWeizhi() {
+		return weizhi;
+	}
+	/**
+	 * 设置：详情
+	 */
+	public void setXiangqing(String xiangqing) {
+		this.xiangqing = xiangqing;
+	}
+	/**
+	 * 获取：详情
+	 */
+	public String getXiangqing() {
+		return xiangqing;
+	}
+	/**
+	 * 设置：价格
+	 */
+	public void setPrice(Float price) {
+		this.price = price;
+	}
+	/**
+	 * 获取：价格
+	 */
+	public Float getPrice() {
+		return price;
+	}
+	/**
+	 * 设置：座位总数
+	 */
+	public void setNumber(Integer number) {
+		this.number = number;
+	}
+	/**
+	 * 获取：座位总数
+	 */
+	public Integer getNumber() {
+		return number;
+	}
+	/**
+	 * 设置：已选座位[用,号隔开]
+	 */
+	public void setSelected(String selected) {
+		this.selected = selected;
+	}
+	/**
+	 * 获取：已选座位[用,号隔开]
+	 */
+	public String getSelected() {
+		return selected;
+	}
+
+}
---
+++ ssmj58m4/src/main/java/com/entity/model/ZixishiModel.java
@@ -0,0 +1,179 @@
+package com.entity.model;
+
+import com.entity.ZixishiEntity;
+
+import com.baomidou.mybatisplus.annotations.TableName;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.util.Date;
+import org.springframework.format.annotation.DateTimeFormat;
+import java.io.Serializable;
+ 
+
+/**
+ * 自习室
+ * 接收传参的实体类  
+ *（实际开发中配合移动端接口开发手动去掉些没用的字段， 后端一般用entity就够用了） 
+ * 取自ModelAndView 的model名称
+ * @author 
+ * @email 
+ * @date 2022-02-24 12:10:04
+ */
+public class ZixishiModel  implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	 			
+	/**
+	 * 图片
+	 */
+	
+	private String tupian;
+		
+	/**
+	 * 规模
+	 */
+	
+	private String guimo;
+		
+	/**
+	 * 位置
+	 */
+	
+	private String weizhi;
+		
+	/**
+	 * 详情
+	 */
+	
+	private String xiangqing;
+		
+	/**
+	 * 价格
+	 */
+	
+	private Float price;
+		
+	/**
+	 * 座位总数
+	 */
+	
+	private Integer number;
+		
+	/**
+	 * 已选座位[用,号隔开]
+	 */
+	
+	private String selected;
+				
+	
+	/**
+	 * 设置：图片
+	 */
+	 
+	public void setTupian(String tupian) {
+		this.tupian = tupian;
+	}
+	
+	/**
+	 * 获取：图片
+	 */
+	public String getTupian() {
+		return tupian;
+	}
+				
+	
+	/**
+	 * 设置：规模
+	 */
+	 
+	public void setGuimo(String guimo) {
+		this.guimo = guimo;
+	}
+	
+	/**
+	 * 获取：规模
+	 */
+	public String getGuimo() {
+		return guimo;
+	}
+				
+	
+	/**
+	 * 设置：位置
+	 */
+	 
+	public void setWeizhi(String weizhi) {
+		this.weizhi = weizhi;
+	}
+	
+	/**
+	 * 获取：位置
+	 */
+	public String getWeizhi() {
+		return weizhi;
+	}
+				
+	
+	/**
+	 * 设置：详情
+	 */
+	 
+	public void setXiangqing(String xiangqing) {
+		this.xiangqing = xiangqing;
+	}
+	
+	/**
+	 * 获取：详情
+	 */
+	public String getXiangqing() {
+		return xiangqing;
+	}
+				
+	
+	/**
+	 * 设置：价格
+	 */
+	 
+	public void setPrice(Float price) {
+		this.price = price;
+	}
+	
+	/**
+	 * 获取：价格
+	 */
+	public Float getPrice() {
+		return price;
+	}
+				
+	
+	/**
+	 * 设置：座位总数
+	 */
+	 
+	public void setNumber(Integer number) {
+		this.number = number;
+	}
+	
+	/**
+	 * 获取：座位总数
+	 */
+	public Integer getNumber() {
+		return number;
+	}
+				
+	
+	/**
+	 * 设置：已选座位[用,号隔开]
+	 */
+	 
+	public void setSelected(String selected) {
+		this.selected = selected;
+	}
+	
+	/**
+	 * 获取：已选座位[用,号隔开]
+	 */
+	public String getSelected() {
+		return selected;
+	}
+			
+}
